a17ac57300092f0a9edbd9845dcfda5dcc7aada9
Abhishek Kumar <akbittu@gmail.com>
2023-07-30 08:48:25 +0530

    Added clone sub-dir only feature

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use reqwest::blocking::Client;
 use serde_json::{Error, Value};
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::fs::File;
 use std::io::Read;
 use std::io::Write;
 use std::io::{copy, BufWriter};
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::process;
 use url::Url;
+use regex::Regex;
 
 fn print_help() {
     println!("Usage:");
@@ -20,6 +23,7 @@ fn print_help() {
     println!(" -h, --help         Show this help message");
     println!(" -v, --version      Show the program version");
     println!(" -u, --url <link>   Github sub-directory URL");
+    println!(" -c, --curdir       Current sub-directory only");
 }
 
 fn print_version() {
@@ -28,7 +32,80 @@ fn print_version() {
     println!("Copyright (c) 2023 Abhishek Kumar licensed under MIT License.");
 }
 
-fn clone_subdir(url: &str) {
+// Define a function that returns a tuple of a hashmap of flags and values and an array of non-flag arguments
+fn parse_args() -> (HashMap<String, String>, Vec<String>) {
+    // Create a hashmap to store the flags and values
+    let mut flags = HashMap::new();
+    // Create an array to store the non-flag arguments
+    let mut args = Vec::new();
+    // Create regular expressions to match different kinds of flags
+    let flag_with_value = Regex::new(r"^[\-\-]{1,2}.+[\=\:].*$").unwrap();
+    let flag_without_value = Regex::new(r"^[\-\-]{1,2}.+$").unwrap();
+
+    // Iterate over the command-line arguments, skipping the first two
+    for arg in env::args() {
+        match arg.as_str() {
+            // If the argument matches a flag with a value, split it and insert it into the hashmap
+            s if flag_with_value.is_match(s) => {
+                let parts = s.splitn(2, |c| c == '=' || c == ':').collect::<Vec<&str>>();
+                let key = parts[0].trim_start_matches('-');
+                let value = parts[1];
+                flags.insert(key.to_string(), value.to_string());
+            }
+            // If the argument matches a flag without a value, insert it into the hashmap with a true value
+            s if flag_without_value.is_match(s) => {
+                let key = s.trim_start_matches('-');
+                flags.insert(key.to_string(), true.to_string());
+            }
+            // Otherwise, push the argument into the array
+            s => {
+                args.push(s.to_string());
+            }
+        }
+    }
+
+    // Return the tuple of hashmap and array
+    (flags, args)
+}
+
+// Define a function that takes a file path and a directory name as arguments and returns a String
+fn strip_dir_path(dir_path: &str, dir_name: &str) -> String {
+    // Create a path from the file path
+    let path = Path::new(dir_path);
+
+    // Split the path into segments
+    let path_segments = path.iter();
+
+    // Create a new path segment
+    let mut stripped_path_segments = PathBuf::new();
+
+    // A flag to indicate if the directory name is found
+    let mut found = false;
+
+    // Loop over the segments until the directory name is found or the iterator is exhausted
+    for segment in path_segments {
+        // Check if the segment matches the directory name
+        if segment == dir_name {
+            // Set the flag to true
+            found = true;
+        }
+        // If the flag is true, append the segment to the new path segment
+        if found {
+            stripped_path_segments.push(segment);
+        }
+    }
+
+    // Collect the remaining segments into a PathBuf
+    let stripped_path: PathBuf = stripped_path_segments.iter().collect();
+
+    // Convert the stripped path to a string
+    let stripped_path_string = stripped_path.to_string_lossy();
+
+    // Return the stripped path string
+    stripped_path_string.into_owned()
+}
+
+fn clone_subdir(url: &str, opt: &mut HashMap<&str, bool>) {
     let url_obj = Url::parse(&url)
         .expect("Invalid URL");
     let path_segments = url_obj.path_segments()
@@ -45,17 +122,24 @@ fn clone_subdir(url: &str) {
     let subdir_path = path_segments.clone().skip(4)
         .collect::<Vec<&str>>().join("/");
 
+    let subdir_name = path_segments.clone().last()
+        .expect("No branch in the URL path");
+    
+    if Path::new(&subdir_path).is_dir() {
+        fs::remove_dir_all(subdir_path.clone()).expect("Failed to delete existing directory");
+    }
+
     let list_content_url = format!(
         "https://api.github.com/repos/{}/{}/contents/{}?ref={}",
         account, repository, subdir_path, branch
     );
 
-    if let Err(e) = list_content(&list_content_url) {
+    if let Err(e) = list_content(&list_content_url, &subdir_name, opt) {
         println!("Failed to list directory content: {}", e);
     }
 }
 
-fn list_content(url: &str) -> Result<(), Error> {
+fn list_content(url: &str, dir: &str, opt: &mut HashMap<&str, bool>) -> Result<(), Error> {
     let client = Client::new();
 
     let mut response = client.get(url).header("User-Agent", " ").send()
@@ -74,23 +158,39 @@ fn list_content(url: &str) -> Result<(), Error> {
                 let dir_url = element["url"].as_str()
                     .expect("Invalid sub-directory URL");
                 
-                if let Err(e) = list_content(dir_url) {
+                if let Err(e) = list_content(dir_url, &dir, opt) {
                     println!("Failed to list sub-directory content: {}", e);
                 }
             } else if file_type == "file" {
                 let file_path = element["path"].as_str()
                     .expect("Invalid filepath in the sub-directory");
                 
-                let file_dir = Path::new(file_path)
-                    .parent()
-                    .expect("Invalid directory path of the file")
-                    .to_str()
-                    .expect("Unable to convert directory path to string");
-                
-                let file_url = element["download_url"].as_str()
-                    .expect("Invalid download file URL");
-                
-                download_file(file_url, file_dir);
+                if opt["curdir"] {
+                    // Get the parent directory of the file path
+                    let file_dir_path = Path::new(file_path)
+                        .parent()
+                        .as_ref()
+                        .and_then(|p| p.to_str())
+                        .unwrap_or("Invalid directory path extracted from sub-file path");
+
+                    let file_dir = &strip_dir_path(file_dir_path, dir);
+                    
+                    let file_url = element["download_url"].as_str()
+                        .expect("Invalid download file URL");
+                    
+                    download_file(file_url, file_dir);
+                } else {
+                    let file_dir = Path::new(file_path)
+                        .parent()
+                        .expect("Invalid directory path of the file")
+                        .to_str()
+                        .expect("Unable to convert directory path to string");
+                    
+                    let file_url = element["download_url"].as_str()
+                        .expect("Invalid download file URL");
+                    
+                    download_file(file_url, file_dir);
+                }
             }
         }
     }
@@ -102,7 +202,7 @@ fn download_file(url: &str, path: &str) {
     let file_name = url.split("/").last().expect("Invalid download file URL");
     let client = Client::new();
     let response = client.get(url).send().expect("Failed to get response");
-    
+
     fs::create_dir_all(path).expect("Failed to create directory");
     
     let file_path = format!("{}/{}", path, file_name);
@@ -117,40 +217,34 @@ fn download_file(url: &str, path: &str) {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let (flags, args) = parse_args();
+
+    println!("{:?}", flags);
+    println!("{:?} {}", args, args.len());
+
+    if flags.contains_key("help") || flags.contains_key("h") {
+        print_help();
+        process::exit(0);
+    }
+
+    if flags.contains_key("version") || flags.contains_key("v") {
+        print_version();
+        process::exit(0);
+    }
+
+    let mut options = HashMap::new();
+
+    if flags.contains_key("curdir") || flags.contains_key("c") {
+        options.insert("curdir", true);
+    } else {
+        options.insert("curdir", false);
+    }
 
     if args.len() > 1 {
-        let option = &args[1];
-
-        match option.as_str() {
-            "-h" | "--help" => print_help(),
-            "-v" | "--version" => print_version(),
-            "-u" | "--url" => {
-                if args.len() >= 3 {
-                    let url_str = match env::args().nth(2) {
-                        Some(url) => url,
-                        None => {
-                            println!("Please provide a URL as an argument");
-                            return;
-                        }
-                    };
-                    clone_subdir(&url_str);
-                } else {
-                    println!("Missing numbers for arguments");
-                }
-            }
-            _ => {
-                let url_str = match env::args().nth(1) {
-                    Some(url) => url,
-                    None => {
-                        println!("Please provide a URL as an argument");
-                        return;
-                    }
-                };
-                clone_subdir(&url_str);
-            }
-        }
+        let url_str = &args[1];
+        clone_subdir(&url_str, &mut options);
     } else {
         println!("No options or arguments provided");
+        process::exit(0);
     }
 }
